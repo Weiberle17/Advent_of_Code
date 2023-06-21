a2db9a8eb5a93ceab269979d249a4b28690b761e
Weiberle17 <maxweiberle63@gmail.com>
2023-06-21 15:18:53 +0200

    feat: Part 2 done

--- 2022/Rost/day_01/src/lib.rs
+++ 2022/Rost/day_01/src/lib.rs
@@ -31,7 +31,18 @@ pub fn process_part1_better(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-  "test".to_string()
+  let mut result = input
+    .split("\n\n")
+    .map(|elf| {
+      elf
+        .lines()
+        .map(|item| item.parse::<u32>().unwrap())
+        .sum::<u32>()
+    })
+    .collect::<Vec<_>>();
+  result.sort_by(|a, b| b.cmp(a));
+  let result: u32 = result.iter().take(3).sum();
+  result.to_string()
 }
 
 #[cfg(test)]
@@ -39,7 +50,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn part_1_test_input() {
+  fn part_1_test() {
     let input = "1000
 2000
 3000
@@ -59,7 +70,7 @@ mod tests {
   }
 
   #[test]
-  fn test_part1_test() {
+  fn part_1_better_test() {
     let input = "1000
 2000
 3000
@@ -76,5 +87,25 @@ mod tests {
 10000";
     let result = process_part1_better(input);
     assert_eq!(result, "24000");
+  }
+
+  #[test]
+  fn part_2_test() {
+    let input = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+    let result = process_part2(input);
+    assert_eq!(result, "45000");
   }
 }
