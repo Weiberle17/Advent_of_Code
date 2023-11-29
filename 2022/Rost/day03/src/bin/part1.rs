e7ad7648b66fe0d03fbc3aba17fb0ddd90c26c7d
Weiberle17 <maxweiberle63@gmail.com>
2023-11-29 20:43:32 +0100

    fix: Just use static input
    Easier to execute

--- 2022/Rost/day03/src/bin/part1.rs
+++ 2022/Rost/day03/src/bin/part1.rs
@@ -2,6 +2,6 @@ use day03::process_part1;
 use std::fs;
 
 fn main() {
-  let file = fs::read_to_string(std::env::args().nth(1).expect("No file provided")).unwrap();
+  let file = fs::read_to_string("input.txt").unwrap();
   println!("{}", process_part1(&file));
 }
--- 2022/Rost/day03/src/bin/part2.rs
+++ 2022/Rost/day03/src/bin/part2.rs
@@ -2,6 +2,6 @@ use day03::process_part2;
 use std::fs;
 
 fn main() {
-  let file = fs::read_to_string(std::env::args().nth(1).expect("No file provided")).unwrap();
-  process_part2(&file);
+  let file = fs::read_to_string("input.txt").unwrap();
+  println!("{}", process_part2(&file));
 }
