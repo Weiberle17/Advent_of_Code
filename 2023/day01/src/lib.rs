b7af8ea6a197e9adf124385fe96887eaa2cea1e8
Weiberle17 <maxweiberle63@gmail.com>
2023-12-01 22:37:39 +0100

    Batman

---
+++ 2023/day01/src/lib.rs
@@ -0,0 +1,30 @@
+use std::cmp;
+
+pub fn process_part1(input: &str) -> u32 {
+  todo!();
+}
+
+pub fn process_part2(input: &str) -> usize {
+  todo!();
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  const INPUT1: &str = "\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+
+  #[test]
+  fn part1_works() {
+    todo!();
+  }
+
+  #[test]
+  fn part2_works() {
+    todo!();
+  }
+}
