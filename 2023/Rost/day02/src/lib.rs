6ca9509dc34c392f091efbc7c364d506d9f439f8
Weiberle17 <maxweiberle63@gmail.com>
2023-12-02 18:24:26 +0100

    feat: Part 1

--- 2023/Rost/day02/src/lib.rs
+++ 2023/Rost/day02/src/lib.rs
@@ -1,5 +1,36 @@
 pub fn process_part1(input: &str) -> usize {
-  todo!();
+  let mut result: usize = 0;
+  let lines = input.lines();
+  for line in lines {
+    let mut iter = line.split(':');
+    let id: Vec<u32> = iter
+      .next()
+      .unwrap()
+      .chars()
+      .filter_map(|c| c.to_digit(10))
+      .collect();
+    let id: String = id.iter().map(|&n| n.to_string()).collect();
+    let id: u32 = id.parse().unwrap();
+    let cubes = iter.next().unwrap().replace(";", ",");
+    let cubes = cubes.split(',');
+    let mut impossible = 0;
+    for cube in cubes {
+      let cube: Vec<&str> = cube.trim().split_whitespace().collect();
+      let number: u32 = cube.first().unwrap().parse().unwrap();
+      let color = cube.last().unwrap();
+      if *color == "red" && number > 12 {
+        impossible += 1;
+      } else if *color == "green" && number > 13 {
+        impossible += 1;
+      } else if *color == "blue" && number > 14 {
+        impossible += 1;
+      }
+    }
+    if impossible == 0 {
+      result += id as usize;
+    }
+  }
+  result
 }
 
 pub fn process_part2(input: &str) -> usize {
@@ -10,9 +41,16 @@ pub fn process_part2(input: &str) -> usize {
 mod tests {
   use super::*;
 
+  const INPUT1: &str = "\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
   #[test]
   fn part1_works() {
-    todo!();
+    assert_eq!(process_part1(INPUT1), 8);
   }
 
   #[test]
