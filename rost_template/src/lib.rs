f47889ddcbbb8790acdb4208d41e8354386bdbee
Weiberle17 <maxweiberle63@gmail.com>
2023-12-03 08:20:47 +0100

    feat: Rust generate template directory

---
+++ rost_template/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn process_part1(input: &str) -> usize {
+  todo!();
+}
+
+pub fn process_part2(input: &str) -> usize {
+  todo!();
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn part1_works() {
+    todo!();
+  }
+
+  #[test]
+  fn part2_works() {
+    todo!();
+  }
+}
