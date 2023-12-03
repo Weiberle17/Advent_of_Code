bdb12bdedaa56917c6246e5442fc11d039573efd
Weiberle17 <maxweiberle63@gmail.com>
2023-12-03 21:59:42 +0100

    WIP Part1

--- 2023/Rost/day03/src/lib.rs
+++ 2023/Rost/day03/src/lib.rs
@@ -1,5 +1,18 @@
-pub fn process_part1(input: &str) -> usize {
+pub fn recursive_num_check(grid: Vec<Vec<char>>) -> usize {
   todo!();
+}
+
+pub fn process_part1(input: &str) -> usize {
+  let mut result: usize = 0;
+  let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+  for i in 0..grid.len() {
+    for j in 0..grid[i].len() {
+      if grid[i][j].is_digit(10) {
+        dbg!(grid[i][j], i, j);
+      }
+    }
+  }
+  result
 }
 
 pub fn process_part2(input: &str) -> usize {
@@ -10,9 +23,21 @@ pub fn process_part2(input: &str) -> usize {
 mod tests {
   use super::*;
 
+  const INPUT1: &str = "\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
   #[test]
   fn part1_works() {
-    todo!();
+    assert_eq!(process_part1(INPUT1), 4361);
   }
 
   #[test]
