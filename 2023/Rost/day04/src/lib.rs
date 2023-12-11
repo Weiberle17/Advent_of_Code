2f0a65f0b3964ae0ed41986c88d68d7f66691ad4
Weiberle17 <maxweiberle63@gmail.com>
2023-12-11 12:50:26 +0100

    feat: Part 2

--- 2023/Rost/day04/src/lib.rs
+++ 2023/Rost/day04/src/lib.rs
@@ -3,6 +3,7 @@ pub struct Card {
   id: usize,
   winners: Vec<usize>,
   numbers: Vec<usize>,
+  amount: usize,
 }
 
 impl Card {
@@ -28,6 +29,7 @@ impl Card {
       id: id.parse().unwrap(),
       winners,
       numbers,
+      amount: 1,
     }
   }
 }
@@ -56,7 +58,26 @@ pub fn process_part1(input: &str) -> usize {
 }
 
 pub fn process_part2(input: &str) -> usize {
-  todo!();
+  let lines = input.lines();
+  let mut cards: Vec<Card> = Vec::new();
+  for line in lines {
+    cards.push(Card::build(line));
+  }
+
+  for i in 0..cards.len() {
+    let mut num_winners = 0;
+    for number in &cards[i].numbers {
+      if cards[i].winners.contains(&number) {
+        num_winners += 1;
+      }
+    }
+    for j in 1..=num_winners {
+      if i + j < cards.len() {
+        cards[i + j].amount += 1 * cards[i].amount;
+      }
+    }
+  }
+  cards.iter().map(|card| card.amount).sum()
 }
 
 #[cfg(test)]
@@ -78,6 +99,6 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
   #[test]
   fn part2_works() {
-    todo!();
+    assert_eq!(process_part2(INPUT1), 30);
   }
 }
