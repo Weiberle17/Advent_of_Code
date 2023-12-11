e544abae71f9cfb409418407f83c383e52e925f8
Weiberle17 <maxweiberle63@gmail.com>
2023-12-11 15:37:41 +0100

    WIP: feat: Part 1

--- 2023/Rost/day05/src/lib.rs
+++ 2023/Rost/day05/src/lib.rs
@@ -1,5 +1,13 @@
 pub fn process_part1(input: &str) -> usize {
-  todo!();
+  let result: usize = 0;
+  let sections: Vec<&str> = input.split("\n\n").collect();
+  let seeds: Vec<usize> = sections[0]
+    .split_whitespace()
+    .flat_map(|x| x.parse::<usize>())
+    .collect();
+  let maps: Vec<Vec<&str>> = sections[1..].into_iter().map(|map| map.lines().collect()).collect();
+  dbg!(seeds, maps);
+  result
 }
 
 pub fn process_part2(input: &str) -> usize {
@@ -10,9 +18,44 @@ pub fn process_part2(input: &str) -> usize {
 mod tests {
   use super::*;
 
+  const INPUT1: &str = "\
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
   #[test]
   fn part1_works() {
-    todo!();
+    assert_eq!(process_part1(INPUT1), 35);
   }
 
   #[test]
